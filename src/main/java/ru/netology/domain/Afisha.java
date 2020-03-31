b522d8e825df06c83a34067da5dc5bd13d6f62c0
Sergey Semenov <gzuppy@gmail.com>
2020-03-31 10:59:11 +0300

    Initial commit

---
+++ src/main/java/ru/netology/domain/Afisha.java
@@ -0,0 +1,12 @@
+package ru.netology.domain;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+
+@AllArgsConstructor
+@Data
+public class Afisha {
+    private int id;
+    private String title;
+    private String genre;
+}
