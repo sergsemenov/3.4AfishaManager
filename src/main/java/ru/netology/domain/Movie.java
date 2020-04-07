ee2d8ff4269c7ebfa03bb1e2d2323d7d22819e54
Sergey Semenov <gzuppy@gmail.com>
2020-04-07 13:40:34 +0300

    NoArgsConstructor added to easy merge

--- src/main/java/ru/netology/domain/Movie.java
+++ src/main/java/ru/netology/domain/Movie.java
@@ -2,7 +2,9 @@ package ru.netology.domain;
 
 import lombok.AllArgsConstructor;
 import lombok.Data;
+import lombok.NoArgsConstructor;
 
+@NoArgsConstructor
 @AllArgsConstructor
 @Data
 public class Movie {
